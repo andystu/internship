37f587da3adc684f6761a0df2d89a928b7c08d31
andystu <andy@stu.edu.tw>
2015-01-12 03:25:57 +0800

    add gender migration for heroku

--- db/migrate/20150106054206_add_info_to_users.rb
+++ db/migrate/20150106054206_add_info_to_users.rb
@@ -3,9 +3,8 @@ class AddInfoToUsers < ActiveRecord::Migration
     add_column :users, :name, :string
     add_column :users, :tel, :string
     add_column :users, :address, :string
-#    add_column :users, :reason, :text
-#    add_column :users, :question, :text
+    add_column :users, :reason, :text
+    add_column :users, :question, :text
     add_column :users, :admin, :boolean, :default => :false
-    add_column :users, :gender, :boolean
   end
 end
